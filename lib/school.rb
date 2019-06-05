f45d2d7ed47bf27f3f5a3d4b36875fbb1e36433b
Renier De La Cruz <me@renierdelacruz.com>
2019-06-05 00:04:28 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,19 +5,6 @@ class School
     @name  = name
     roster = {}
   end
-  
-  def roster
-    @roster
-  end
-  
-# #   def self.all 
-# #     @@all
-# #   end
-
-#   def add_student
-#   end
-      
-  
 end
 
 # school = School.new("Bayside High School")
